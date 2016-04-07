87cf47afb1ad767fc3d0df560e52e6f4c1b2b8de
Nina Roby <ninaroby.usa@gmail.com>
2016-04-06 18:55:57 -0700

    test to verify the creation of a line item and content of the response returned by the request.

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -12,3 +12,9 @@ class StoreControllerTest < ActionController::TestCase
   end
 
 end
+
+  test "markup needed for store.js.coffee is in place" do
+    get :index
+    assert_select '.store .entry img', 3
+    assert_select '.entry input[type=submit', 3
+  end
