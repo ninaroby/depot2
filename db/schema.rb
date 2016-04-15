2ca480ebbfe68f26cf3e110d9f30cc09cfd54a4a
Nina Roby <ninaroby.usa@gmail.com>
2016-04-14 20:24:50 -0700

    created model and database to hold administrators' usernames and passwords

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160407020324) do
+ActiveRecord::Schema.define(version: 20160415025427) do
 
   create_table "carts", force: true do |t|
     t.datetime "created_at"
@@ -45,6 +45,13 @@ ActiveRecord::Schema.define(version: 20160407020324) do
     t.text     "description"
     t.string   "image_url"
     t.decimal  "price"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
