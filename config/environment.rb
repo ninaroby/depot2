48a9284a5fd9e7e1fb0eafe78bb3d96d60e62d0e
Nina Roby <ninaroby.usa@gmail.com>
2016-04-24 17:11:55 -0700

    email configuration

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,17 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Depot::Application.initialize!
+
+Depot::Application.configure  do
+    config.action_mailer.delivery_method = :smtp
+
+    config.action_mailer.smtp_settings = {
+        address: "smtp.gmail.com",
+        port: 587,
+        domain: "gmail.com",
+        authentication: 'plain',
+        user_name: "ninaroby.usa@gmail.com",
+        password: "Fanc7Pant5",
+        enable_starttls_auto: true
+    }
+end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,6 +15,8 @@ Depot::Application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_deliveries = true
+  #config.action_mailer.delivery_method = :smtp
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -29,4 +31,15 @@ Depot::Application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  config.action_mailer.delivery_method = :sendmail
+
+  config.action_mailer.smtp_settings = {
+        address: "smtp.gmail.com",
+        port: 587,
+        domain: "gmail.com",
+        authentication: 'plain',
+        user_name: "ninaroby.usa@gmail.com",
+        password: "Fanc7Pant5",
+        enable_starttls_auto: true
+    }
 end
