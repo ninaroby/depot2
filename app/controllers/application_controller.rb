e49b251c8a6488f8017940f38dade4446eb6b0a6
Nina Roby <ninaroby.usa@gmail.com>
2016-04-28 21:00:20 -0700

    Adds notification of the user logged in and creates user docs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,20 @@
 class ApplicationController < ActionController::Base
     before_action :authorize
+    before_action :check_for_login
+
         # Prevent CSRF attacks by raising an exception.
         # For APIs, you may want to use :null_session instead.
         # protect_from_forgery with: :exception
 
+   
+       
     protected
+
+    def check_for_login
+        if session[:user_id] and User.find_by(id: session[:user_id])
+            @logged_in_user_name = User.find_by(id: session[:user_id]).name
+        end
+    end
 
     def authorize
         unless User.find_by(id: session[:user_id]) or User.count.zero?
