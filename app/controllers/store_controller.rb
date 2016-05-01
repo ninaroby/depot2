fd90c5d96065c6bb1aeab5a8ee02b7f968cbd268
Nina Roby <ninaroby.usa@gmail.com>
2016-04-30 19:56:14 -0700

    set default locale for application, created translation files for text fields, currency amounts, errors and model names and added button to change language

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -4,8 +4,11 @@ class StoreController < ApplicationController
     include CurrentCart
     before_action :set_cart
     def index
+      if params[:set_locale]
+        redirect_to store_url(locale: params[:set_locale])
+      else
       	@products = Product.order(:title)
-
+      end
 
       	if session[:counter].nil?
 
--- config/routes.rb
+++ config/routes.rb
@@ -15,14 +15,16 @@ Depot::Application.routes.draw do
   resources :carts
 
   get "store/index"
-  resources :products
+  resources :products do
+    get :who_bought, on: :member
+  end
 
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-root 'store#index', as: 'store'
-
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store', via: :all
+  end
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
