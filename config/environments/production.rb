f5d255a15ad166a027dc4c8670c21af2c364ef02
Nina Roby <ninaroby.usa@gmail.com>
2016-05-05 20:32:39 -0700

    added email configurations to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,15 @@ Depot::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+      config.action_mailer.delivery_method = :smtp
+
+    config.action_mailer.smtp_settings = {
+        address: "smtp.gmail.com",
+        port: 587,
+        domain: "gmail.com",
+        authentication: 'login',
+        user_name: "littlewebchair@gmail.com",
+        password: "H@ppy123**",
+        enable_starttls_auto: true
+    }
 end
